dd256b73a4af2342d5fac7faea63745f9ae2bb1d
João Vitor Soares Bispo <joaovitor646260@Gmail.com>
2025-11-25 16:25:39 -0300

    commit final

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-import React from 'react'
-import { Header } from '../header/Header'
-import Chat from '../componentes/Chat'
-import { Banner } from '../componentes/Banner'
-import { Itens } from '../componentes/Itens'
-import { Categorias } from '../componentes/Categorias'
-import { Footer } from '../componentes/Footer'
-import { Depoimentos } from '../componentes/Depoimentos'
+  import React from 'react'
+  // Certifique-se de que os caminhos dos imports estão corretos
+  import { Banner } from '../componentes/Banner'
+  import { Itens } from '../componentes/Itens'
+  import { Categorias } from '../componentes/Categorias'
+  import { Footer } from '../componentes/Footer'
+  import { Depoimentos } from '../componentes/Depoimentos'
+  import Chat from '../componentes/Chat'
 
- const App = () => {
-  return (
-    <section>
-      <Banner/>
-      <Categorias/>
-      <Itens/>
-      <Depoimentos/>
-      <Chat/>
-      <Footer/>
-    </section>
-  )
-}
+  const App = () => {
+    return (
+      <section className="relative">
+        <Banner/>
+        <Categorias/>
+        <Itens/>
+        <Depoimentos/>
+        <Footer/>
+        
+        {/* O Chat flutua, então pode ficar aqui no final */}
+        <Chat/>
+      </section>
+    )
+  }
 
-export default App
+  export default App
